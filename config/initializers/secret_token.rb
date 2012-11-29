bff66de726066e0342882f6c5e4257f4bf0d9e91
pan_liber_ubuntu_server <pmq2001@gmail.com>
2012-11-29 23:54:01 +0800

    328->329

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Empty321::Application.config.secret_token = '14efd2f02d8cc6b28b448ff5f2b2f24560c73742792e68a098b6354cad4233ffc8b8adbe90608c353c2c0e25b8fe8b45a05f72e4fb3683f491cf25d2fe0cc666'
+Empty321::Application.config.secret_token = 'fcd6d46c61daaf7a1abb869f7281a7b8f08f63c578f3e21ab094c3e4aaef9323fb04dd72fb27038dab9935147290f1659397211a79a8d435f550f278f5d43d1e'
