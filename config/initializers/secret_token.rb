bf1cb87a328b2329b4a5bd59eb7b39977f1ef01c
Minqi Pan <pmq2001@gmail.com>
2013-01-13 21:52:42 -0500

    329->32.11

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Empty321::Application.config.secret_token = 'fcd6d46c61daaf7a1abb869f7281a7b8f08f63c578f3e21ab094c3e4aaef9323fb04dd72fb27038dab9935147290f1659397211a79a8d435f550f278f5d43d1e'
+Empty321::Application.config.secret_token = '5a89a87d580dfa8da06b1116881bf58d87c49b7e5cd17383ef3fed89b7400340f2d63daa87bee85067340075eb8438b56479fdd3b13961d55b792a3a6e76d29c'
