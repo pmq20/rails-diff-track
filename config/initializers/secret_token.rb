b73faa1650d2c28707f4192e513e2c0eb6faeed1
P.S.V.R <pmq2001@gmail.com>
2013-03-25 11:26:44 +0800

    3213

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Empty321::Application.config.secret_token = '5a89a87d580dfa8da06b1116881bf58d87c49b7e5cd17383ef3fed89b7400340f2d63daa87bee85067340075eb8438b56479fdd3b13961d55b792a3a6e76d29c'
+Empty321::Application.config.secret_token = '0e647fc8dc96175371c4101ff5ea9cb1d2cb6e8dfccf4a4160944688aca9630ba17fd74343919aff9ab8d2a17e576e5ff6c3670f0aed74b4141734e8d530fb0f'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Empty321::Application.config.session_store :cookie_store, key: '_empty321_session'
+Empty321::Application.config.session_store :cookie_store, key: '_Empty321_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
